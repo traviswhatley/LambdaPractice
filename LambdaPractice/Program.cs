0bcf0d4d24114bb5cee8e9204f72f6911bf2066f
Travis Whatley <travis.whatley121@gmail.com>
2014-07-01 15:31:19 -0600

    Lambda Practice Final

--- LambdaPractice/Program.cs
+++ LambdaPractice/Program.cs
@@ -19,52 +19,37 @@ namespace LambdaPractice
             {
                 Console.WriteLine(a);
             }
+
+            //single line solution
+            Console.WriteLine(string.Join(", ", kayakProducts));
             //declare a variable shoeProducts and set it equal to all products that contain the word "Shoes"
             var shoeProducts = products.Where(x => x.Contains("Shoes"));
             //print the shoeProducts to the console using a foreach loop.
-            foreach (var b in shoeProducts)
-            {
-                Console.WriteLine(b);
-            }
+            Console.WriteLine(string.Join(", ", shoeProducts));
             //declare a variable ballProducts and set it equal to all the products that have ball in the name.
-            var ballProducts = products.Where(x => x.Contains("ball")).ToString();
+            var ballProducts = products.Where(x => x.ToLower().Contains("ball")).ToList();
             //print the ballProducts to the console using a foreach loop.
-            foreach (var c in ballProducts)
-            {
-                Console.WriteLine(c);
-            }
+            Console.WriteLine(string.Join(", ", ballProducts));
             //sort ballProducts alphabetically and print them to the console.
-            ballProducts = ballProducts.OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(", ", ballProducts.OrderBy(x => x)));
             //add six more items to the products list using .add().
-            products.Add("Hockey Mask");
-            products.Add("Hockey Stick");
-            products.Add("Hockey Puck");
-            products.Add("Hockey Net");
-            products.Add("Hockey Gloves");
-            porducts.Add("Hockey Pads");
+            products.Add("Hockey Mask"); products.Add("Hockey Stick"); products.Add("Hockey Puck"); products.Add("Hockey Net"); products.Add("Hockey Gloves");
+            products.Add("Hockey Pads");
             //print the product with the longest name to the console using the .First() extension.
             Console.WriteLine(products.OrderByDescending(x => x.Length).First());
             //print the product with the shortest name to the console using the .First() extension.
             Console.WriteLine(products.OrderBy(x => x.Length).First());
             //print the product with the 4th shortest name to the console using an index (you must convert the results to a list using .ToList()).
-            products.OrderBy(x => x.Length).ToString();
-            Console.WriteLine(products[3]);
+            Console.WriteLine(products.OrderBy(x => x.Length).Skip(3).Take(1).First());
             //print the ballProduct with the 2nd longest name to the console using an index(you must convert the results to a list using .ToList()).
-            ballProducts.OrderByDescending(x => x.Length).ToString();
-            Console.WriteLine(ballProducts[1]);
+            Console.WriteLine(ballProducts.OrderByDescending(x => x.Length).ToList()[1]);
             //declare a variable reversedProducts and set it equal to all products ordered by the longest word first. (use the OrderByDecending() extension).
             var reversedProducts = products.OrderByDescending(x => x.Length);
             //print out the reversedProducts to the console using a foreach loop.
-            foreach (var d in reversedProducts)
-            {
-                Console.WriteLine(reversedProducts);
-            }
+            Console.WriteLine(string.Join(", ", products.OrderByDescending(x => x.Length).ThenBy(x => x)));
             //print out all the products ordered by the longest word first using the OrderByDecending() extension and a foreach loop.  
             //You will not use a variable to store your list
-            foreach (var d in products)
-            {
-                Console.WriteLine(products.OrderByDescending(x => x.Length).First());
-            }
+            Console.WriteLine(string.Join(", ", products.OrderByDescending(x => x.Length)));
 
             Console.ReadKey();
         }
